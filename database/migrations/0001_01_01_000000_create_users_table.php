b439cf9c90276368d0198c176468b812a912f79b
dwiti-2611 <118741899+dwiti-2611@users.noreply.github.com>
2024-05-24 19:00:44 +0530

    made controllers

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('type')->default(0);
+            /* Users: 0=>User, 1=>Admin */
             $table->rememberToken();
             $table->timestamps();
-        });
+          });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
