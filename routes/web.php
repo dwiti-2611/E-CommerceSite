e4284bd244d1363b2e8b011ebdfa1cf799249131
dwiti-2611 <118741899+dwiti-2611@users.noreply.github.com>
2024-05-27 15:23:49 +0530

    basic route file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authentications\LoginBasic;
+use App\Http\Controllers\authentications\RegisterBasic;
+use App\Http\Controllers\authentications\ForgotPasswordBasic;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route for displaying the login form
+Route::get('/', [Login::class, 'index'])->name('Login');
+
+// Route for handling login POST request
+Route::post('/authentications/login', [LoginBasic::class, 'login'])->name('Login.post');
+
+// Route for logout
+Route::post('/logout', [LoginBasic::class, 'logout'])->name('logout');
+
+// Dashboard routes based on user type
+Route::middleware('auth')->group(function () {
+  Route::get('/admin/Home', [AdminController::class, 'adminHome'])->name('admin.Home');
+  Route::get('/user/Home', [UserController::class, 'userHome'])->name('user.Home');
 });
+
+// Route for displaying the registration form
+Route::get('/authentications/Register', [Register::class, 'index'])->name('Register');
+
+// Route for handling registration POST request
+Route::post('/auth/Register', [Register::class, 'register'])->name('Register.post');
