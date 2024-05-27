d71d4d67c5b6ebfe6a0e043ad00882c8eee30d58
dwiti-2611 <118741899+dwiti-2611@users.noreply.github.com>
2024-05-27 14:13:19 +0530

    made models

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -17,9 +20,19 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'firstname',
+        'lastname',
+        'type',
+        'address',
+        'country',
+        'state',
+        'city',
+        'phonenumber',
+        'gender',
+        'zipcode',
     ];
 
     /**
@@ -43,5 +56,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    protected function type(): Attribute
+    {
+        return new Attribute(get: fn($value) => ['user', 'admin'][$value]);
     }
 }
